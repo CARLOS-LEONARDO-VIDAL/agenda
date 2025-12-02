c43503760e3939287464ba12aace4ad732472b0d
Carlos Leonardo Vidal Flota <simon343810@gmail.com>
2025-12-02 00:22:09 -0600

    aaa

--- src/JASON.JS/server.js
+++ src/JASON.JS/server.js
@@ -1,20 +1,19 @@
 import axios from "axios";
-
-//const baseUrl = 'https://backend2-s86u.onrender.com/api/persons';
-const baseUrl= "/api/persons"
+ 
+const baseUrl = 'https://backend2-s86u.onrender.com/api/persons';
+ 
 const getAll = () =>
   axios.get(baseUrl).then(response => response.data);
-
+ 
 const create = (newObject) =>
   axios.post(baseUrl, newObject).then(response => response.data);
-
+ 
 const update = (id, updatedObject) =>
   axios.put(`${baseUrl}/${id}`, updatedObject).then(response => response.data);
-
+ 
 const remove = (id) =>
   axios.delete(`${baseUrl}/${id}`);
-
+ 
 export default { getAll, create, update, remove };
-
 
 
